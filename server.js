96bdc6e066ad10865cebf3fc132deba0d2ee1e21
dbosing <d.bosing154@mybvc.ca>
2025-02-26 00:49:01 -0700

    Fix server.js

--- server.js
+++ server.js
@@ -16,3 +16,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
