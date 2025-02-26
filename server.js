ad364946e2d42b81e9843562606da485b6d15191
dbosing <d.bosing154@mybvc.ca>
2025-02-26 01:09:51 -0700

    fix port issue for Railway

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const serviceRoutes = require("./routes/services");
 const popupRoutes = require("./routes/popups");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
